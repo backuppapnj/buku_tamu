df96d433595eb4ee1bbe5bec5a73bd70e359309a
Moohard <dian.kuba13@gmail.com>
2026-03-05 05:32:30 +0800

    fix(ui): center align icons in home page cards

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -17,7 +17,9 @@
                     <div class="row g-5">
                         <div class="col-md-6">
                             <a href="/pengunjung" class="card border border-primary border-hover border-dashed bg-light-primary text-center p-10 h-100 card-hover">
-                                <i class="ki-duotone ki-people text-primary fs-3x mb-5"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></i>
+                                <div class="d-flex justify-content-center mb-5">
+                                    <i class="ki-duotone ki-people text-primary fs-3x"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></i>
+                                </div>
                                 <h3 class="text-primary fw-bolder mb-2">Pengunjung</h3>
                                 <div class="text-gray-600 fs-7">Untuk masyarakat umum yang berkunjung ke kantor</div>
                                 <div class="btn btn-sm btn-primary fw-bold mt-5 w-100">Isi Data</div>
@@ -25,7 +27,9 @@
                         </div>
                         <div class="col-md-6">
                             <a href="/tamu" class="card border border-success border-hover border-dashed bg-light-success text-center p-10 h-100 card-hover">
-                                <i class="ki-duotone ki-badge text-success fs-3x mb-5"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></i>
+                                <div class="d-flex justify-content-center mb-5">
+                                    <i class="ki-duotone ki-badge text-success fs-3x"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></i>
+                                </div>
                                 <h3 class="text-success fw-bolder mb-2">Tamu</h3>
                                 <div class="text-gray-600 fs-7">Untuk tamu dari instansi/perusahaan yang berkunjung</div>
                                 <div class="btn btn-sm btn-success fw-bold mt-5 w-100">Isi Data</div>
